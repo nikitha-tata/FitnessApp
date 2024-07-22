a6c6fa902dcebc3d4c31923f0b87f18cea6fcd6c
Nikitha Tata <tnikitha1996@gmail.com>
2024-07-22 16:23:22 -0500

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Nikitha Tata on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
