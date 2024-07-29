7e0378a49a4d9f3d823ad4240f40fceddda91fa3
Nikitha Tata <tnikitha1996@gmail.com>
2024-07-29 12:56:08 -0500

    Fitness Home Screen: Activity Card Ui.

--- FitnessApp/Home/HomeView.swift
+++ FitnessApp/Home/HomeView.swift
@@ -11,9 +11,16 @@ struct HomeView: View {
     @State var calories: Int = 100
     @State var goal: Int = 30
     @State var stand: Int = 50
+    
+    let mockActivities: [Activity] = [
+        Activity(id: 0, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .green, amount: "6,200"),
+        Activity(id: 1, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .red, amount: "812"),
+        Activity(id: 2, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .blue, amount: "9,900"),
+        Activity(id: 3, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .purple, amount: "55,000"),
+        ]
     var body: some View {
         ScrollView(showsIndicators: false) {
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Welcome")
                     .font(.largeTitle)
                     .fontWeight(.semibold)
@@ -62,8 +69,39 @@ struct HomeView: View {
                         .padding(.all, 40)
                         
                 }
-                .padding()
+                .padding(.horizontal)
+                Spacer()
             }
+            .padding()
+            
+            HStack {
+                Text("Fitness Activity")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                Spacer()
+                Button(action: {
+                    print("Show More")
+                }, label: {
+                    Text("Show More")
+                        .padding(.all, 10)
+                        .foregroundStyle(.white)
+                        .background(.blue)
+                        .cornerRadius(8.0)
+                    
+                })
+            }
+            .padding(.horizontal)
+            
+            LazyVGrid(columns: Array(repeating: GridItem(spacing: 8), count: 2)) {
+                ForEach(mockActivities, id: \.id) { activity in
+                    ActivityCardView(activity: activity)
+                }
+            }
+            .padding(.horizontal)
+            
+//            ActivityCardView()
+//                .padding(.horizontal)
+           
         }
     }
 }
